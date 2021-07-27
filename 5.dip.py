68be875e7eed38190dd40c78e072cf28cefcc6d7
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2021-07-27 11:35:12 -0300

    Modificando dip

--- 5.dip.py
+++ 5.dip.py
@@ -6,23 +6,39 @@ Dependências devem ser feitas sobre abstrações, não sobre implementações c
 """
 
 
+from abc import ABC, abstractmethod
+
+class AbstractStatsReporter(ABC):
+    def set_player(self, player):
+        self.char = player
+    
+    @abstractmethod
+    def report(self):
+        pass
+
 class Player:
-    def __init__(self, name):
-        self.stats = StatsReporter(self)
+    def __init__(self, name, statsReporter: AbstractStatsReporter):
         self.__name = name
         self.__hp = 100
         self.__speed = 1
+        self.reporter = statsReporter
+        self.reporter.set_player(self)
 
     def hp(self):
         return self.__hp
 
     def name(self):
         return self.__name
+    
+    def report(self):
+        self.reporter.report()
 
-class StatsReporter:
-    def __init__(self, char: Player):
-        self.char = char
-
+class StatsReporter(AbstractStatsReporter):
     def report(self):
         print(f'Name:{self.char.name()}')
         print(f'HP:{self.char.hp()}')
+
+class StatsReporterWithColor(AbstractStatsReporter):
+    def report(self):
+        print(f'\033[31mName:{self.char.name()}\033[0;0m')
+        print(f'\033[32mHP:{self.char.hp()}\033[31m')
