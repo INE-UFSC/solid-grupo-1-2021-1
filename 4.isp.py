e31f6e239f170d7e2f6554771fc3d80b753d11ec
MarcoA555 <marcoantonio.machadodearruda@gmail.com>
2021-07-27 11:06:09 -0300

    4 exemplo isp

--- 4.isp.py
+++ 4.isp.py
@@ -4,43 +4,34 @@ Crie interfaces que são específicas. Clientes não devem depender de interface
 """
 from abc import ABC
 
-class IJanela(ABC):
-    def maximizar(self):
-        raise NotImplementedError
-    
-    def minimizar(self):
-        raise NotImplementedError
-    
-    def mostrar_menu(self):
-        raise NotImplementedError
-        
+class IJanela(ABC): 
     def fechar(self):
         raise NotImplementedError
 
-class JanelaTamanhoFixo(IJanela):
+class IJanelaTamanhoVariavel(IJanela, ABC):
+    def minimizar(self):
+        pass
+
     def maximizar(self):
         pass
 
-    def minimizar(self):
+class IJanelaComMenu(IJanela, ABC):
+    def mostrar_menu(self):
         pass
-    
+
+class JanelaTamanhoFixo(IJanelaComMenu):    
     def mostrar_menu(self):
         pass
     
     def fechar(self):
         pass
 
-class JanelaSemMenu(IJanela):
+class JanelaSemMenu(IJanelaTamanhoVariavel):
     def maximizar(self):
         pass
 
     def minimizar(self):
         pass
     
-    def mostrar_menu(self):
-        pass
-    
     def fechar(self):
         pass
-
-
